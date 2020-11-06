fc499fb2ef3124d5bf1801b8fd6276ca22cf8a3c
Jane Smith <jame.smith@mths.ca>
2020-11-06 03:54:07 +0000

    init

---
+++ floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by Mr. Coxall
+# Created on June 2020
+# This program uses modules
+
+import math
+
+
+def main():
+    # this function returns the integer portion of a float
+
+    print("The integer part of 12.9 is {0}.".format(math.floor(12.9)))
+
+
+if __name__ == "__main__":
+    main()
